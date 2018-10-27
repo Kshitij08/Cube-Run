88a32c17d8572020ec68f8599c7e77ed9eef4723
Kshitij Gajapure <kshitijiitgn@gmail.com>
2018-10-27 17:12:31 +0530

    Added unity files

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+	public void Quit()
+	{
+		Debug.Log ("QUIT");
+		Application.Quit ();
+	}
+
+}
